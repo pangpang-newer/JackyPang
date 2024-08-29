72dd4eb709f7821542980d0a42e4e608cd26ffd5
Jacky_Peng <s1304980@live.hkmu.edu.hk>
2024-08-29 19:54:12 +0800

    Update blogs.js

--- webjs/blogs.js
+++ webjs/blogs.js
@@ -1,21 +1,24 @@
-
-let date = new Date();
+ import {reblog} from './article.js';
+ 
+ console.log(reblog);
 let blogHTML='';
-
+const date=new Date();
 reblog.forEach(blogs => {
   blogHTML += `
 
   <h1 class="title">${blogs.title}</h1>
   <p class="maincontent"> ${blogs.content} </p>
   <div class=" imgcontainer">  <img src="${blogs.picture}"  class="pimg" alt="东莞之旅"> </div>
-  <p class="date"> ${date.toDateString()} </p>
+  <p class="date"> ${date.toDateString()}  </p>
     <hr class="hrline">
  `; 
     
 });
 
-let blog_blocks = document.querySelector('.blogbox').innerHTML=blogHTML;
+document.querySelector('.blogbox').innerHTML=blogHTML;
 
-
-console.log(blogHTML);
-
+window.addEventListener('scroll',()=>{
+  let line = document.querySelector('.progressline').style.width;
+      line = window.scrollY/(document.body.scrollHeight-window.innerHeight)*100;
+  document.querySelector('.progressline').style.width=line + "%";
+});
