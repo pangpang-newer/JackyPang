36601545f3d3438198c8d30265d641809d76154b
Jacky_Peng <s1304980@live.hkmu.edu.hk>
2024-12-26 17:09:29 +0800

    Update movies.js

--- webjs/movies.js
+++ webjs/movies.js
@@ -2,18 +2,27 @@ export const movies =[
 {
 name:'Oppenheimer',
 pic:'pics/Oppenheimer.jpg',
+category:'美劇',
+popuparity:'9.5',
+episodes:'1-3',
 description:'印第安人的一支欧塞奇族，从祖先的土地上被驱逐到了美国俄克拉何马州一块贫瘠之地，谁曾想这里居然发现了石油。欧塞奇人借此一跃成为时尚新贵的同时，也给自身带来了杀机。在此之后，许多白人涌入这片流淌着黑金的土地，不择手段攫取利益。一战退伍兵欧内斯特（莱昂纳多·迪卡普里奥 Leonardo DiCaprio 饰）为了生计辗转来此，投奔已经贵为名流舅舅威廉·黑尔（罗伯特·德尼罗 Robert De Niro 饰）。威廉和原住民关系密切，黑白通吃，然而和善的外表下包藏着贪婪的蛇蝎之心。他暗示欧内斯特追求富有的印第安女子莫莉（莉莉·格莱斯顿 Lily Gladstone 饰），而真正觊觎的则是她们家族所拥有的财富。短短数年间，数十名印第安人意外身亡。君子无罪，怀璧其罪…… 　　本片根据大卫·格兰的畅销著作改编。',
 source:'https://ddys.pro/oppenheimer/'
 
 },{
     name:'flowermoon',
     pic:'pics/flowermoon.jpg',
+    category:'美劇',
+    popuparity:'9.0',
+    episodes:'1-3',
     description:'x',
     source:'https://ddys.pro/killers-of-the-flower-moon/'
 
 },{
     name:'bastarden',
     pic:'pics/bastarden.jpg',
+    category:'美劇',
+    popuparity:'9.0',
+    episodes:'1-3',
     description:'x',
     source:'https://ddys.pro/bastarden/'
 
@@ -21,6 +30,9 @@ source:'https://ddys.pro/oppenheimer/'
 ,{
     name:'alita_battle_angel',
     pic:'pics/alita_battle_angel.jpg',
+    category:'美劇',
+    popuparity:'9.0',
+    episodes:'1-3',
     description:'x',
     source:'https://ddys.pro/alita-battle-angel/ '
 
@@ -28,16 +40,138 @@ source:'https://ddys.pro/oppenheimer/'
 ,{
     name:'beautiful_mind',
     pic:'pics/beautiful_mind.jpg',
+    popuparity:'9.0',
+    episodes:'1-3',
     description:'x',
     source:'https://ddys.pro/a-beautiful-mind/ '
 
 }
-,{
-    name:'flowermoon',
-    pic:'pics/flowermoon.jpg',
-    description:'x',
-    source:'https://ddys.pro/killers-of-the-flower-moon/'
 
-}
+
 ];
+
+export const books = [ 
+    {   img:'../books/pictures/Clear Thinking.jpg',
+        bookname:'Clear Thinking',
+        rating:'4.0',
+        PDFsource:'https://mega.nz/file/nnwhnSzB#Uxlra13YJmOFBu8SswQMLXmue45QJ3ezUCTUMVVX4qM',
+        EPUBsource:'https://mega.nz/file/3yJWURpJ#PbmdynTYUZ2uioGz4tr6d7nk_J-iy42TeOi_QZ2fSRo',
+        pdffile:'4.29',
+        epubfile:'0.958'
+    },{   
+        img:'../books/pictures/Talk Like TED.jpg',
+        bookname:'Talk Like TED',
+        rating:'5.0',
+        PDFsource:'https://mega.nz/file/36BTQDIA#2eVyO4-Jot58lPrIEbFyq2hHfX3-8yDifkqqWGeT1gQ',
+        EPUBsource:'https://mega.nz/file/K6IUgR4S#ASBNzi6CVA4uMY94M8X1hMk3SIzFumKJDyWz34omWbM',
+        pdffile:'2.76',
+        epubfile:'2.35'
+    },{  
+        img:'../books/pictures/High Output Management.jpg', 
+        bookname:'High Output Management',
+        rating:'4.5',
+        PDFsource:'https://mega.nz/file/qvQQkLqT#d9rRuRquHVnxrESOnXzwtaBJnRFGr6tqkUe4KX4JzR0',
+        EPUBsource:'https://mega.nz/file/CqYTDYhQ#1NRGMAqGLBmq2tXeui7veUWtFc9CzQfBXJyLWRRHQq4',
+        pdffile:'4.20',
+        epubfile:'2.45'
+
+    },{  
+        img:'../books/pictures/Atomic Habits.jpg', 
+        bookname:'Atomic Habits',
+        rating:'4.0',
+        PDFsource:'https://mega.nz/file/eixDxaxY#jQK2HwoWFAbpiHPEfP-FXfocsT9eYDMHy5ZgSm11yhg',
+        EPUBsource:'https://mega.nz/file/uzZmSYBJ#kNvFSWhRXovofiebxOUyHKrOmaHA5AuJeYB4FKB4ZuE',
+        pdffile:'3.91',
+        epubfile:'2.90'
+    },{  
+        img:'../books/pictures/Behave.jpg', 
+        bookname:'Behave',
+        rating:'4.0',
+        PDFsource:'https://mega.nz/file/inwk2aZS#0iPc0XmSeOE7s6CfINPsUdu0uuaTtaJ2em5BQJDVhSI',
+        EPUBsource:'https://mega.nz/file/32xBkRJJ#RGk3_uYDmbMvTBrHXN-8fYt7LBa0f8WFFZoFyj8iV70',
+        pdffile:'17.1',
+        epubfile:'7.27'
+    },{  
+        img:'../books/pictures/Principles.jpg', 
+        bookname:'Principles',
+        rating:'5.0',
+        PDFsource:'https://mega.nz/file/W6I0yRzR#3wYuEz-hRKP5dsJWrP-WiFFfSAKCv4x2HeSCVwWW7l8',
+        EPUBsource:'https://mega.nz/file/Onxhxbya#hHX8AHIEPiEY5NBqIS-ipSpB9PcFOEpalFIoWcySUiM',
+        pdffile:'1.63',
+        epubfile:'0.593'
+    },{  
+        img:'../books/pictures/Zero to One.jpg', 
+        bookname:'Zero to One',
+        rating:'5.0',
+        PDFsource:'https://mega.nz/file/W6I0yRzR#3wYuEz-hRKP5dsJWrP-WiFFfSAKCv4x2HeSCVwWW7l8',
+        EPUBsource:'https://mega.nz/file/ynBAhKwB#wtQOF3vU_-vJKUqFnVuTglsgZ6L-IXEtxwXQ0x_u5gI',
+        pdffile:'1.63',
+        epubfile:'0.593'
+    },
+    
+    /*chinese book*/
+    {
+        img:'../books/pictures/我们仨.jpg',  
+        bookname:'我们仨',
+        rating:'5.0',
+        PDFsource:'https://mega.nz/file/qro3XIhT#iJuZKqxoSN6sjv5Flv-JYzqH_BBwd93ovOQU7MbSo3o',
+        EPUBsource:'https://mega.nz/file/en40EBIZ#KGqf6fWlZ98QMkQNpG1hH_8up5kS4t7r48oT2ifdD0o',
+        pdffile:'7.11',
+        epubfile:'0.17'
+
+    }, 
+    { 
+        img:'../books/pictures/被讨厌的勇气.jpg',  
+        bookname:'被讨厌的勇气',
+        rating:'5.0',
+        PDFsource:'https://mega.nz/file/37hgDCxC#qto8cyfXMsRIaAa8DXFaqntVhdkIEmHlFrmRO0NB088',
+        EPUBsource:'https://mega.nz/file/O751SIhY#YrfWUKw02-D91MQ2b4nlw84t9EDro0kr7le4fq2d8-Q',
+        pdffile:'4.29',
+        epubfile:'1'
+    },{  
+        img:'../books/pictures/贫穷的质感.jpg', 
+        bookname:'贫穷的质感',
+        rating:'5.0',
+        PDFsource:'https://mega.nz/file/uzRiWYaY#5Zku5Fh379Y3XlzKHiiIrDYEMuWmRdFlqIMGPjEM1I8',
+        EPUBsource:'https://mega.nz/file/a6hXXBLA#mBwu3SNdDE9h1slQuS5P47YWShqJGsmi9M3iT8X-cVE',
+        pdffile:'4.29',
+        epubfile:'1'
+
+    },{ 
+        img:'../books/pictures/晚熟的人.jpg',  
+        bookname:'晚熟的人',
+        rating:'5.0',
+        PDFsource:'https://mega.nz/file/qnIkyAgb#vS9SLi4Tz0-AmO-sPyc4gaeBKIcY4xYeJEo6Nwl4zhA',
+        EPUBsource:'https://mega.nz/file/P6oyXDpD#127DcQkP4g6V0GCSo-tLTMNhKgWssloobpLAEyWL8-w',
+        pdffile:'4.29',
+        epubfile:'1'
+
+    },{ 
+        img:'../books/pictures/我的几何人生.jpg',  
+        bookname:'我的几何人生',
+        rating:'5.0',
+        PDFsource:'../books/Chinese/PDF/我的几何人生.pdf',
+        EPUBsource:'https://mega.nz/file/v3Jy1RJB#YOO7N5GpLjU_ZXZDhQeNsyakwU7M9yFQx1tdQulFhBE',
+        pdffile:'4.29',
+        epubfile:'1'
+    },{ 
+        img:'../books/pictures/乌合之众.jpg',  
+        bookname:'烏合之眾',
+        rating:'5.0',
+        PDFsource:'https://mega.nz/file/TvwF1A7Q#QFjjXhnLPCvi39n0w88QRTsuLnH2E7PPKbCOajOzXzY',
+        EPUBsource:'https://mega.nz/file/qzAClYBS#nQ8XvREq_ydkkVxaZnCtNurDZVxvNSAqW7qfje5Kq-0',
+        pdffile:'4.29',
+        epubfile:'1'
+    },{ 
+        img:'../books/pictures/制造宠物.jpg',  
+        bookname:'制造宠物',
+        rating:'5.0',
+        PDFsource:'https://mega.nz/file/Dy4FALTZ#1DSXef7DRyqdBWuJhpHmsns-jbj1yJtV7ylLuHXVE7Q',
+        EPUBsource:'https://mega.nz/file/GioGBAAC#rpzcZEIDTgEomVo5rGUQTEL_83QENwncp1S5iB9M3gI',
+        pdffile:'4.29',
+        epubfile:'1'
+
+    }
+    ]
+    
 
