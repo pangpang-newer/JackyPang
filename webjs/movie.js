d097bb8cb9650425e7eb4e6ab4fc21e51a221bfc
Jacky_Peng <s1304980@live.hkmu.edu.hk>
2025-01-03 16:58:19 +0800

    Update movie.js

--- webjs/movie.js
+++ webjs/movie.js
@@ -1,8 +1,10 @@
 import {movies} from './movies.js';
 
+function  moviesgenerator(movies_blocks){
+
 let movieHTML='';
 
-movies.forEach(movie => {
+movies_blocks.forEach(movie => {
   
   
      movieHTML += `
@@ -18,14 +20,63 @@ movies.forEach(movie => {
                  
 });
 
+let movies_data=document.querySelector('.Submovies_container');
+movies_data.innerHTML=movieHTML;
 
-let movies_blocks=document.querySelector('.Submovies_container');
-movies_blocks.innerHTML=movieHTML;
+ };
 
+ moviesgenerator(movies);
 
+let getresult;
+document.getElementById('searchbutton').addEventListener('click',()=>{
+  getresult=document.getElementById('UserSearchTarget').value.toLowerCase();
+    const filterpage = movies.filter(submovie=>submovie.name.toLowerCase()===getresult);
+    console.log(filterpage)
+    if (filterpage.length == 0 ){
+      moviesgenerator(movies);
+    }
+    else{
+      moviesgenerator(filterpage)
+    }
+ })
 
 window.addEventListener('scroll',()=>{
   let line = document.querySelector('.progressline').style.width;
       line = window.scrollY/(document.body.scrollHeight-window.innerHeight)*100;
   document.querySelector('.progressline').style.width=line + "%";
 });
+
+
+/* .........searching bar ..........................................................
+const search = document.querySelector('.search').value;
+const rebookhtml = document.querySelector('.Submovies_container').innerHTML;
+console.log(search);
+map(search, book => {if( book.name.includes(search){
+  document.querySelector('.Submovies_container').innerHTML =`<div class="content"> 
+             <img src="${movie.pic}" class="backgroundimg"></img>
+            <div class="star textdetail"><i class="fa-solid fa-star StarDetail"></i></div>
+
+            <div class="shawbox"></div>
+                <div class="label textdetail"><span>${movie.category}</span> <span>${movie.popuparity}</span></div>
+                <div class="statu textdetail"><a class="clickblock" href="${movie.source}">${movie.name}</a><span>${movie.episodes}</span> </div>
+            </div> ` `
+}});
+
+
+
+
+
+....................................................................................*/
+
+
+
+
+
+
+
+
+
+
+
+
+
