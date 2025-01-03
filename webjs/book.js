4ef25604de78da15a22bc8f2f3f01441da45c31e
Jacky_Peng <s1304980@live.hkmu.edu.hk>
2025-01-03 16:57:17 +0800

    Update book.js

--- webjs/book.js
+++ webjs/book.js
@@ -1,15 +1,15 @@
-import {books} from './movies.js';
-
+import {books, movies} from './movies.js';
+function bookgenerator(bookdatabase){
 let BookHTML='';
 
-books.forEach(book => {
+bookdatabase.forEach(book => {
      BookHTML += `
      <div class="book"> 
-     <div class="leftside"><a href="#" class=""> <img src="${book.img}" class="bookimg" ></a></div>
+     <div class="leftside"><a href="#" class=""> <img src="${book.img}" alt="pic error" class="bookimg" ></a></div>
      <div class="rightside">
        <div class="rating"><i class="fa-regular fa-star"></i> <span class="Marks">${book.rating}</span> <span class="Maxmarks">/5.0</span></div>
-       <div class="pdf"><i class="fa-solid fa-file-arrow-down"></i><a class="link"  href="${book.PDFsource}"><span>Download(PDF,${book.pdffile}MB)</span></a></div>
-       <div class="epub"><i class="fa-solid fa-file-arrow-down"></i><a class="link"  href="${book.EPUBsourcesource}"><span>Download(EPUB,${book.epubfile}MB)</span></a></div>
+       <div class="pdf"><a class="link"  href="https://drive.google.com/uc?id=${book.PDFid}"><span>Download(PDF,${book.pdffile}MB)</span></a></div>
+       <div class="epub"></i><a class="link"  href="https://drive.google.com/uc?id=${book.EPUBid}"><span>Download(EPUB,${book.epubfile}MB)</span></a></div>
 
 
      </div>           
@@ -20,8 +20,43 @@ books.forEach(book => {
 
 let Book_blocks=document.querySelector('.bookscontainer');
 Book_blocks.innerHTML=BookHTML;
+};
+bookgenerator(books);
+let getresult;
+document.getElementById('booksearchbutton').addEventListener('click',()=>{
+  getresult=document.getElementById('SearchBookData').value.toLowerCase();
+  console.log(getresult);
+    const filteredbook = books.filter(subook=>subook.bookname.toLowerCase().includes(getresult));
+    if (filteredbook.length == 0 ){
+      bookgenerator(books);
+    }
+    else{
+      bookgenerator(filteredbook)
+    }
+ })
 
-console.log('BookHTML');
+ 
+const inputs = document.querySelectorAll('input');
+for ( const input of inputs ){
+  input.addEventListener('click', (event) => {
+    const elem = event.currentTarget;
+    if (elem.checked) {
+      const label = elem.parentNode.textContent.trim(); 
+      console.log(label);
+      const filteredbook = books.filter(subook => subook.category === label);
+      if (filteredbook.length == 0 ){
+        bookgenerator(books);
+      }
+      else{
+        bookgenerator(filteredbook)}
+    }
+    
+  });
+
+}
+
+
+
 
 window.addEventListener('scroll',()=>{
    let line = document.querySelector('.progressline').style.width;
