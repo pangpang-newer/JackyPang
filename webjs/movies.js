bbe894eef72e17b316ff5f024e70cbf7fc657733
Jacky_Peng <s1304980@live.hkmu.edu.hk>
2025-01-09 20:09:50 +0800

    Update movies.js

--- webjs/movies.js
+++ webjs/movies.js
@@ -1,252 +1,235 @@
-export const movies =[
-{
-name:'Oppenheimer',
-pic:'pics/Oppenheimer.jpg',
-category:'美劇',
-popuparity:'9.5',
-episodes:'1-3',
-description:'印第安人的一支欧塞奇族，从祖...',
-source:'https://ddys.pro/oppenheimer/'
-
-},{
-    name:'flowermoon',
-    pic:'pics/flowermoon.jpg',
-    category:'美劇',
-    popuparity:'9.0',
-    episodes:'1-3',
-    description:'x',
-    source:'https://ddys.pro/killers-of-the-flower-moon/'
-
-},{
-    name:'bastarden',
-    pic:'pics/bastarden.jpg',
-    category:'美劇',
-    popuparity:'9.0',
-    episodes:'1-3',
-    description:'x',
-    source:'https://ddys.pro/bastarden/'
-
-}
-,{
-    name:'alita_battle_angel',
-    pic:'pics/alita_battle_angel.jpg',
-    category:'美劇',
-    popuparity:'9.0',
-    episodes:'1-3',
-    description:'x',
-    source:'https://ddys.pro/alita-battle-angel/ '
-
-}
-,{
-    name:'beautiful_mind',
-    pic:'pics/beautiful_mind.jpg',
-    popuparity:'9.0',
-    episodes:'1-3',
-    description:'x',
-    source:'https://ddys.pro/a-beautiful-mind/ '
-
-}
-
-
-];
-
-export const books = [ 
-    {   category:'Fiction',
-        img:'../pictures/Clear Thinking.jpg',
-        bookname:'Clear Thinking',
-        rating:'4.0',
-        PDFid:'1E8aoNBkwQyKzgHCNSpniiahgySd9alT1',
-        EPUBid:'1x0LuITdkAhRLtGpF49YcPeWDiFcZZ5gO',
-        pdffile:'4.29',
-        epubfile:'0.958'
-    },{   
-        category:'Fiction',
-        img:'../pictures/Talk Like TED.jpg',
-        bookname:'Talk Like TED',
-        rating:'5.0',
-        PDFid:'1qemBIzJhVrvnfaMjoEdhP9afMKSJQO4-',
-        EPUBid:'1sQE2-1IJKznxRgD7wGjloH5kZeb_QzeW',
-        pdffile:'2.76',
-        epubfile:'2.35'
-    },{  
-        category:'Fiction',
-        img:'../pictures/High Output Management.jpg', 
-        bookname:'High Output Management',
-        rating:'4.5',
-        PDFid:'1uIS2L1eCoyGlhHS8YwGc7PnngRDS_Gq1',
-        EPUBid:'1D5SZI3uKI26Zd6CGViZy9eHQ4xL9Riox',
-        pdffile:'4.20',
-        epubfile:'2.45'
-
-    },{ 
-        category:'Fiction',
-        img:'../pictures/Atomic Habits.jpg', 
-        bookname:'Atomic Habits',
-        rating:'4.0',
-        PDFid:'1egA--vFxX1PAcNFCP-n6zrivbg8TeOAp',
-        EPUBid:'1vSMEJZZNSYRRLlW7CRezHMQQqDDx8vn3',
-        pdffile:'3.91',
-        epubfile:'2.90'
-    },{  
-        category:'Fiction',
-        img:'../pictures/Behave.jpg', 
-        bookname:'Behave',
-        rating:'4.0',
-        PDFid:'1Jsl55q8mh_1Pz06G9uub6XPCb7ik02q_',
-        EPUBid:'1M7r6WMQ-mHw8qP-oDwpr4zsQ5bjK-Piu',
-        pdffile:'17.1',
-        epubfile:'7.27'
-    },{  
-        category:'Fiction',
-        img:'../pictures/Principles.jpg', 
-        bookname:'Principles',
-        rating:'5.0',
-        PDFid:'1bCKHx--_-X5S24nUmVLMx2sxW9WE8NmF',
-        EPUBid:'1USNu_0VXtnHNBVp4B25aEhOpVr1UkApD',
-        pdffile:'1.63',
-        epubfile:'0.593'
-    },{  
-        category:'Fiction',
-        img:'../pictures/Zero to One.jpg', 
-        bookname:'Zero to One',
-        rating:'5.0',
-        PDFid:'https://mega.nz/file/W6I0yRzR#3wYuEz-hRKP5dsJWrP-WiFFfSAKCv4x2HeSCVwWW7l8',
-        EPUBid:'1DbkrS0gtVh_imGYp-ypu8IRmW-tZKJ0M',
-        pdffile:'1.63',
-        epubfile:'0.593'
-    },{
-        category:'Economics', 
-        img:'../pictures/Your Next Five Moves.jpg',  
-        bookname:'Your Next Five Moves',
-        rating:'5.0',
-        PDFid:'13gEjBQP5951jFa39jY3E6VYDXY9nBgv',
-        EPUBid:'18S5FcSlqGosUDCJfvndY1OqtXhHT3WtW',
-        pdffile:'1.98',
-        epubfile:'0.797'
-    },{ 
-        category:'Fiction',
-        img:'../pictures/The Stranger.jpg',  
-        bookname:'The Stranger',
-        rating:'3.5',
-        PDFid:'1_Hp6UFh8ZnUwFgxBe27xtyG1DFfmNfGI',
-        EPUBid:'1bzi7zEEgJEzdWFiLroiSaJ1LQSsIdZyp',
-        pdffile:'4.29',
-        epubfile:'1'
-
-    },{
-        category:'Economics', 
-        img:'../pictures/Early Retirement Extreme.jpg',  
-        bookname:'Early Retirement Extreme',
-        rating:'5.0',
-        PDFid:'1qx30DTWx6c1mvFeypwQMKBL2xuOBHDEN',
-        EPUBid:'17HiomOq_pe8L17wX8YJldTPBy9UNdULB',
-        pdffile:'2.16',
-        epubfile:'0.681'
-    },{ 
-        category:'Economics',
-        img:'../pictures/Four Thousand Weeks.jpg',  
-        bookname:'Four Thousand Weeks',
-        rating:'4.0',
-        PDFid:'1YTMbJRmMsABnA7ZHMgyLvcx60oEYsjhs',
-        EPUBid:'1CIERIf5YMx4J9BANCqy3Qsm7sMKEJrSB',
-        pdffile:'1.87 ',
-        epubfile:'0.678 '
-
-    },{
-        category:'Self-Help', 
-        img:'../pictures/Supercommunicators.jpg',  
-        bookname:'Supercommunicators',
-        rating:'5.0',
-        PDFid:'1ce2g4Q96BPybbIaTZPdiYzE6g1712nmQ',
-        EPUBid:'1VMPjXtP26Hq3Iowj53GBjbVAQuDRASU-',
-        pdffile:'15.8        ',
-        epubfile:'12.5        '
-    },{ 
-        category:'Economics',
-        img:'../pictures/The Psychology of Money.jpg',  
-        bookname:'The Psychology of Money',
-        rating:'5.0',
-        PDFid:'1HQdEg4Mx7-JJlXvl8AxOdehMJYVQzTb-',
-        EPUBid:'1buLQFnQOmUWQyaptNbNPX8Tw3ZbFwInU',
-        pdffile:'9.05',
-        epubfile:'1.14'
-    },{ 
-        category:'Philosophy',
-        img:'../pictures/The Parasitic Mind.jpg',  
-        bookname:'The Parasitic Mind',
-        rating:'5.0',
-        PDFid:'1LH1BUhWtZW8NrY2ANGVP_iJEsthwYzLN',
-        EPUBid:'136aYuna0O1dhaVl7sYR6IJTbIjDvlbX2',
-        pdffile:'2.97',
-        epubfile:'1.53'
+export const books = [
+    {
+        numberid: '1',
+        category: 'Fiction',
+        img: 'AgACAgUAAx0Eflp52gACWptnelIyUasD7lnj8_uXzhdBSsnsPAACVb0xG-P60FcwjfNpNHxDqQEAAwIAA3cAAzYE',
+        bookname: 'Clear Thinking',
+        rating: '4.0',
+        PDFid: '1E8aoNBkwQyKzgHCNSpniiahgySd9alT1',
+        EPUBid: '1x0LuITdkAhRLtGpF49YcPeWDiFcZZ5gO',
+        pdffile: '4.29',
+        epubfile: '0.958'
+    },
+    {
+        numberid: '2',
+        category: 'Fiction',
+        img:'AgACAgUAAx0Eflp52gACWqdnelJEwR0yZJgmH8InI9gnjsN2PAACW70xG-P60FfgY90uOq7_1AEAAwIAA3gAAzYE',
+        bookname: 'Talk Like TED',
+        rating: '5.0',
+        PDFid: '1qemBIzJhVrvnfaMjoEdhP9afMKSJQO4-',
+        EPUBid: '1sQE2-1IJKznxRgD7wGjloH5kZeb_QzeW',
+        pdffile: '2.76',
+        epubfile: '2.35'
+    },
+    {
+        numberid: '3',
+        category: 'Fiction',
+        img: 'AgACAgUAAx0Eflp52gACWqNnelI_SpqFxFMlqtQ1Hk7dPcSZcQACWb0xG-P60Fcmm88vEFmbzgEAAwIAA3gAAzYE',
+        bookname: 'High Output Management',
+        rating: '4.5',
+        PDFid: '1uIS2L1eCoyGlhHS8YwGc7PnngRDS_Gq1',
+        EPUBid: '1D5SZI3uKI26Zd6CGViZy9eHQ4xL9Riox',
+        pdffile: '4.20',
+        epubfile: '2.45'
+    },
+    {
+        numberid: '4',
+        category: 'Fiction',
+        img: 'AgACAgUAAx0Eflp52gACWr1nelKC66AESlN5wCsg_5e8Non9KAACab0xG-P60FfGCIxOmisz0gEAAwIAA3gAAzYE',
+        bookname: 'Atomic Habits',
+        rating: '4.0',
+        PDFid: '1egA--vFxX1PAcNFCP-n6zrivbg8TeOAp',
+        EPUBid: '1vSMEJZZNSYRRLlW7CRezHMQQqDDx8vn3',
+        pdffile: '3.91',
+        epubfile: '2.90'
+    },
+    {
+        numberid: '5',
+        category: 'Fiction',
+        img: 'AgACAgUAAx0Eflp52gACWplnelItVkIFrB31w_xBMpUa8p0ZlwACU70xG-P60FfCVwz171irjAEAAwIAA3gAAzYE',
+        bookname: 'Behave',
+        rating: '4.0',
+        PDFid: '1Jsl55q8mh_1Pz06G9uub6XPCb7ik02q_',
+        EPUBid: '1M7r6WMQ-mHw8qP-oDwpr4zsQ5bjK-Piu',
+        pdffile: '17.1',
+        epubfile: '7.27'
+    },
+    {
+        numberid: '6',
+        category: 'Fiction',
+        img: 'AgACAgUAAx0Eflp52gACWqNnelI_SpqFxFMlqtQ1Hk7dPcSZcQACWb0xG-P60Fcmm88vEFmbzgEAAwIAA3gAAzYE',
+        bookname: 'Principles',
+        rating: '5.0',
+        PDFid: '1bCKHx--_-X5S24nUmVLMx2sxW9WE8NmF',
+        EPUBid: '1USNu_0VXtnHNBVp4B25aEhOpVr1UkApD',
+        pdffile: '1.63',
+        epubfile: '0.593'
+    },
+    {
+        numberid: '7',
+        category: 'Fiction',
+        img: 'AgACAgUAAx0Eflp52gACWsNneli_BOm9IrhbmLMF4ylxDMiLGQACh70xG-P60Fej4rRyKQy8RwEAAwIAA3gAAzYE',
+        bookname: 'Zero to One',
+        rating: '5.0',
+        PDFid: 'https://mega.nz/file/W6I0yRzR#3wYuEz-hRKP5dsJWrP-WiFFfSAKCv4x2HeSCVwWW7l8',
+        EPUBid: '1DbkrS0gtVh_imGYp-ypu8IRmW-tZKJ0M',
+        pdffile: '1.63',
+        epubfile: '0.593'
+    },
+    {
+        numberid: '8',
+        category: 'Economics',
+        img: 'AgACAgUAAx0Eflp52gACWpdnelFGXvFkviguuIpSjy6zZSqD5wACTL0xG-P60FcrRaUy4URrrwEAAwIAA3gAAzYE',
+        bookname: 'Your Next Five Moves',
+        rating: '5.0',
+        PDFid: '13gEjBQP5951jFa39jY3E6VYDXY9nBgv',
+        EPUBid: '18S5FcSlqGosUDCJfvndY1OqtXhHT3WtW',
+        pdffile: '1.98',
+        epubfile: '0.797'
+    },
+    {
+        numberid: '9',
+        category: 'Fiction',
+        img: 'AgACAgUAAx0Eflp52gACWqtnelJKC9XtkNBPsuowSZvWEmGKcQACXb0xG-P60Fc-4IkrseoNEAEAAwIAA3gAAzYE',
+        bookname: 'The Stranger',
+        rating: '3.5',
+        PDFid: '1_Hp6UFh8ZnUwFgxBe27xtyG1DFfmNfGI',
+        EPUBid: '1bzi7zEEgJEzdWFiLroiSaJ1LQSsIdZyp',
+        pdffile: '4.29',
+        epubfile: '1'
+    },
+    {
+        numberid: '10',
+        category: 'Economics',
+        img: 'AgACAgUAAx0Eflp52gACWp1nelI2AcrdSH5L8irENTIyDZe-JgACVr0xG-P60FfLGci6sWOjvQEAAwIAA3gAAzYE',
+        bookname: 'Early Retirement Extreme',
+        rating: '5.0',
+        PDFid: '1qx30DTWx6c1mvFeypwQMKBL2xuOBHDEN',
+        EPUBid: '17HiomOq_pe8L17wX8YJldTPBy9UNdULB',
+        pdffile: '2.16',
+        epubfile: '0.681'
+    },
+    {
+        numberid: '11',
+        category: 'Economics',
+        img: 'AgACAgUAAx0Eflp52gACWp9nelI5-cm_K-pihJZzB4bmC1ExlQACV70xG-P60FfnxkOtmkteqwEAAwIAA3gAAzYE',
+        bookname: 'Four Thousand Weeks',
+        rating: '4.0',
+        PDFid: '1YTMbJRmMsABnA7ZHMgyLvcx60oEYsjhs',
+        EPUBid: '1CIERIf5YMx4J9BANCqy3Qsm7sMKEJrSB',
+        pdffile: '1.87 ',
+        epubfile: '0.678 '
+    },
+    {
+        numberid: '12',
+        category: 'Self-Help',
+        img: 'AgACAgUAAx0Eflp52gACWqVnelJB3hY8vIPFkHxFebvpKfX_DQACWr0xG-P60FdQ40KrsWrYtgEAAwIAA3gAAzYE',
+        bookname: 'Supercommunicators',
+        rating: '5.0',
+        PDFid: '1ce2g4Q96BPybbIaTZPdiYzE6g1712nmQ',
+        EPUBid: '1VMPjXtP26Hq3Iowj53GBjbVAQuDRASU-',
+        pdffile: '15.8        ',
+        epubfile: '12.5        '
+    },
+    {
+        numberid: '13',
+        category: 'Economics',
+        img: 'AgACAgUAAx0Eflp52gACWsVnell5MUix846dPxBFBF-dlcPLNgACi70xG-P60FdCUt3ZlBs_YgEAAwIAA3gAAzYE',
+        bookname: 'The Psychology of Money',
+        rating: '5.0',
+        PDFid: '1HQdEg4Mx7-JJlXvl8AxOdehMJYVQzTb-',
+        EPUBid: '1buLQFnQOmUWQyaptNbNPX8Tw3ZbFwInU',
+        pdffile: '9.05',
+        epubfile: '1.14'
+    },
+    {
+        numberid: '14',
+        category: 'Philosophy',
+        img: 'AgACAgUAAx0Eflp52gACWqlnelJIuA9-k97G8UqKr2D5t7cSbgACXL0xG-P60FcTXmPFeFNXmgEAAwIAA3gAAzYE',
+        bookname: 'The Parasitic Mind',
+        rating: '5.0',
+        PDFid: '1LH1BUhWtZW8NrY2ANGVP_iJEsthwYzLN',
+        EPUBid: '136aYuna0O1dhaVl7sYR6IJTbIjDvlbX2',
+        pdffile: '2.97',
+        epubfile: '1.53'
     },
     /*chinese book*/
-    {    category:'Biography',
-        img:'../pictures/我们仨.jpg',  
-        bookname:'我们仨',
-        rating:'5.0',
-        PDFid:'1thh27eAndScJiUMDPEcivlBDHWUiFGen',
-        EPUBid:'1Ju4IKY1GQZFyv-RqR-3DMWrw5Hozd8rY',
-        pdffile:'7.11',
-        epubfile:'0.17'
-
-    }, 
-    {  category:'Fiction',
-        img:'../pictures/被讨厌的勇气.jpg',  
-        bookname:'被讨厌的勇气',
-        rating:'5.0',
-        PDFid:'1W6KKOJIaupqYWXLCa5-VMpgi6HY5Gsjj',
-        EPUBid:'17_eAKN6sT25SDjIZ63Qn7nelLyx3yxkw',
-        pdffile:'4.29',
-        epubfile:'1'
-    },{  
-        category:'Fiction',
-        img:'../pictures/贫穷的质感.jpg', 
-        bookname:'贫穷的质感',
-        rating:'5.0',
-        PDFid:'16qtrWeRwhClMHDVQE7xOkbeq6Rh900VQ',
-        EPUBid:'14BDTcCHjhzIw4G10DVIZGk9AyXfAaQ90',
-        pdffile:'4.29',
-        epubfile:'1'
-
-    },{ 
-        category:'Fiction',
-        img:'../pictures/晚熟的人.jpg',  
-        bookname:'晚熟的人',
-        rating:'5.0',
-        PDFid:'1T2ednrlINbb5tj9VW1uSBPX0PRaHvrSC',
-        EPUBid:'1J2ck-R_II0bB2_oaRP19fh91tZIXeuv1',
-        pdffile:'4.29',
-        epubfile:'1'
-
-    },{ 
-        category:'Biography',
-        img:'../pictures/我的几何人生.jpg',  
-        bookname:'我的几何人生',
-        rating:'5.0',
-        PDFid:'../books/Chinese/PDF/我的几何人生.pdf',
-        EPUBid:'1bHOfDgdDzMnyCKaJ5EOdyYNICbg64Yrh',
-        pdffile:'4.29',
-        epubfile:'1'
-    },{
-        category:'Fiction', 
-        img:'../pictures/乌合之众.jpg',  
-        bookname:'烏合之眾',
-        rating:'5.0',
-        PDFid:'1uTXmoMA9_zDR7Y-Lga90UvC9KEgQCDXZ',
-        EPUBid:'1RMJH-u0wThK-bGyBw2LWjXbCBnvGgzEp',
-        pdffile:'4.29',
-        epubfile:'1'
-    },{ 
-        category:'Fiction',
-        img:'../pictures/制造宠物.jpg',  
-        bookname:'制造宠物',
-        rating:'5.0',
-        PDFid:'1_Hp6UFh8ZnUwFgxBe27xtyG1DFfmNfGI',
-        EPUBid:'1bzi7zEEgJEzdWFiLroiSaJ1LQSsIdZyp',
-        pdffile:'4.29',
-        epubfile:'1'
-
+    {
+        numberid: '15',
+        category: 'Biography',
+        img: 'AgACAgUAAx0Eflp52gACWrdnelJ59hT9FJCt783DPSL9gsdSgAACZL0xG-P60FcmqXmdqQ1grAEAAwIAA3gAAzYE',
+        bookname: '我们仨',
+        rating: '5.0',
+        PDFid: '1thh27eAndScJiUMDPEcivlBDHWUiFGen',
+        EPUBid: '1Ju4IKY1GQZFyv-RqR-3DMWrw5Hozd8rY',
+        pdffile: '7.11',
+        epubfile: '0.17'
+    },
+    {
+        numberid: '16',
+        category: 'Fiction',
+        img: 'AgACAgUAAx0Eflp52gACWq9nelJsJ2Ft4F8RgPKot7TnIGOckQACYL0xG-P60Fdqew79N2kEwQEAAwIAA3gAAzYE',
+        bookname: '被讨厌的勇气',
+        rating: '5.0',
+        PDFid: '1W6KKOJIaupqYWXLCa5-VMpgi6HY5Gsjj',
+        EPUBid: '17_eAKN6sT25SDjIZ63Qn7nelLyx3yxkw',
+        pdffile: '4.29',
+        epubfile: '1'
+    },
+    {
+        numberid: '17',
+        category: 'Fiction',
+        img: 'AgACAgUAAx0Eflp52gACWrFnelJw274T4NriFYHZu2PaTyyifgACYb0xG-P60Ff-25mOeweWygEAAwIAA3cAAzYE',
+        bookname: '贫穷的质感',
+        rating: '5.0',
+        PDFid: '16qtrWeRwhClMHDVQE7xOkbeq6Rh900VQ',
+        EPUBid: '14BDTcCHjhzIw4G10DVIZGk9AyXfAaQ90',
+        pdffile: '4.29',
+        epubfile: '1'
+    },
+    {
+        numberid: '18',
+        category: 'Fiction',
+        img: 'AgACAgUAAx0Eflp52gACWrNnelJ0OL0u3sQUq8u25pCoqiPe7AACYr0xG-P60FdTtXlbDNcAAYsBAAMCAAN4AAM2BA',
+        bookname: '晚熟的人',
+        rating: '5.0',
+        PDFid: '1T2ednrlINbb5tj9VW1uSBPX0PRaHvrSC',
+        EPUBid: '1J2ck-R_II0bB2_oaRP19fh91tZIXeuv1',
+        pdffile: '4.29',
+        epubfile: '1'
+    },
+    {
+        numberid: '19',
+        category: 'Biography',
+        img: 'AgACAgUAAx0Eflp52gACWrVnelJ2Dpxw-SJP-ipKkHYe6XGVFgACY70xG-P60Fcu1z1XvHYT9QEAAwIAA3gAAzYE',
+        bookname: '我的几何人生',
+        rating: '5.0',
+        PDFid: '../books/Chinese/PDF/我的几何人生.pdf',
+        EPUBid: '1bHOfDgdDzMnyCKaJ5EOdyYNICbg64Yrh',
+        pdffile: '4.29',
+        epubfile: '1'
+    },
+    {
+        numberid: '20',
+        category: 'Fiction',
+        img: 'AgACAgUAAx0Eflp52gACWrlnelJ95lloMOJ0t7EVyJwiC1BDvAACZr0xG-P60Fdb2_mTXxeYEQEAAwIAA3gAAzYE',
+        bookname: '烏合之眾',
+        rating: '5.0',
+        PDFid: '1uTXmoMA9_zDR7Y-Lga90UvC9KEgQCDXZ',
+        EPUBid: '1RMJH-u0wThK-bGyBw2LWjXbCBnvGgzEp',
+        pdffile: '4.29',
+        epubfile: '1'
+    },
+    {
+        numberid: '21',
+        category: 'Fiction',
+        img: 'AgACAgUAAx0Eflp52gACWrtnelKAmaSrYgu5SH_aEjZ5VcEtBAACZ70xG-P60FcvFd09AmYOGgEAAwIAA3gAAzYE',
+        bookname: '制造宠物',
+        rating: '5.0',
+        PDFid: '1_Hp6UFh8ZnUwFgxBe27xtyG1DFfmNfGI',
+        EPUBid: '1bzi7zEEgJEzdWFiLroiSaJ1LQSsIdZyp',
+        pdffile: '4.29',
+        epubfile: '1'
     }
-    ]
+];
     
